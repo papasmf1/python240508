20fc75ea55d4730757fee79a9ecc6019c44285e9
papasmf1 <papasmf1@gmail.com>
2024-05-08 09:51:40 +0900

    반복구문 함수 연습

--- ifelse.py
+++ ifelse.py
@@ -30,3 +30,31 @@ for k,v in fruits.items():
 print("---range()함수---")
 print(list(range(2000,2025)))
 print(list(range(1,32)))
+
+print("---리스트 함축---")
+lst = list(range(1,11))
+print(lst)
+print([i**2 for i in lst if i > 5])
+d = {100:"apple", 200:"kiwi"}
+print([v.upper() for v in d.values()])
+
+print("---필터링---")
+lst = [10, 25, 30]
+iterL = filter(None, lst)
+for item in iterL:
+    print(item)
+
+#함수를 정의
+def getBiggerThan20(i):
+    return i > 20 
+
+print("---필터링 한 경우---")
+iterL = filter(getBiggerThan20, lst)
+for item in iterL:
+    print(item)
+
+print("---람다함수를 사용---")
+iterL = filter(lambda x:x>20, lst)
+for item in iterL:
+    print(item)
+
