2f4550c3bbfcca0a57337be0a08a7a39ea90439e
papasmf1 <papasmf1@gmail.com>
2024-05-09 11:39:13 +0900

    당근마켓 크롤링 완료

--- web2.py
+++ web2.py
@@ -9,6 +9,8 @@ response = requests.get(url)
 
 soup = BeautifulSoup(response.text, "html.parser")
 
+#파일에 저장
+f = open("danggn.txt", "wt", encoding="utf-8")
 posts = soup.find_all("div", attrs={"class":"card-desc"})
 for post in posts:
     titleElem = post.find("h2", attrs={"class":"card-title"})
@@ -19,6 +21,9 @@ for post in posts:
     addr = addrElem.text.strip()
     #파이썬 3.6부터는 f-string사용 가능 
     print(f"{title}, {price}, {addr}")
+    f.write(f"{title}, {price}, {addr}\n")
+
+f.close() 
 
 #  <div class="card-desc">
 #       <h2 class="card-title">에어팟 프로1 팔.아요</h2>
