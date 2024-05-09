fb572d1163b146a7ba8b9423243375afefeafeb0
papasmf1 <papasmf1@gmail.com>
2024-05-09 13:45:54 +0900

    오늘의 유머 크롤링

---
+++ 오늘의유머.py
@@ -0,0 +1,31 @@
+# coding:utf-8
+from bs4 import BeautifulSoup
+import urllib.request
+import re 
+
+#User-Agent를 조작하는 경우(아이폰에서 사용하는 사파리 브라우져의 헤더) 
+hdr = {'User-agent':'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/603.1.23 (KHTML, like Gecko) Version/10.0 Mobile/14E5239e Safari/602.1'}
+
+for n in range(1,11):
+        #오늘의 유머 베스트 게시판
+        data ='https://www.todayhumor.co.kr/board/list.php?table=bestofbest&page=' + str(n)
+        print(data)
+        #웹브라우져 헤더 추가 
+        req = urllib.request.Request(data, headers = hdr)
+        data = urllib.request.urlopen(req).read()
+        #한글이 깨지는 경우 
+        page = data.decode('utf-8', 'ignore')
+        soup = BeautifulSoup(page, 'html.parser')
+        list = soup.find_all('td', attrs={'class':'subject'})
+        #<td class="subject">
+        #<a href="/board/view.php?table=bestofbe">쌍둥이는 아이폰 쓰면 안되는 이유</a>
+
+        for item in list:
+                try:
+                        title = item.find("a").text.strip()
+                        #print(title)
+                        if (re.search('한국', title)):
+                                print(title.strip())        
+                except:
+                        pass
+        
