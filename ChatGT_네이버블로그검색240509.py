b83d89211910de75fde9404f1750d5f0c71064cb
papasmf1 <papasmf1@gmail.com>
2024-05-10 09:10:00 +0900

    네이브 블로그 크롤링

--- ChatGT_네이버블로그검색240509.py
+++ ChatGT_네이버블로그검색240509.py
@@ -21,26 +21,26 @@ ws.append(["블로그명", "블로그주소", "글 제목", "포스팅 날짜"])
 for page in range(1, 100):
     url = f'https://search.naver.com/search.naver?where=view&sm=tab_jum&query={search_keyword}&start={page * 10 - 9}'
 
-   
-# <div class="fds-ugc-block-mod-list swvwrquoALMoh11A2Inw"><div class="fds-ugc-block-mod swvwrquoALMoh11A2Inw"><div class="_keep_wrap fds-article-simple-box swvwrquoALMoh11A2Inw"><div 
-#<a nocr="1" href="https://blog.naver.com/gywnstjswkd/223429700587" class="WA5NokotVTFlsTmcyqjA fds-comps-right-image-text-title" target="_blank" data-cb-target="'SYS-0000000035525141.90000003_00000000000000340572ABEB'" data-cb-trigger="true"><span class="yzABCkcX2xTtNICntyz1"><mark>맥북에어</mark> m2 실버 스펙 장점 감면 받는법</span></a>
-    posts = soup.find_all('div', {'class':'fds-ugc-block-mod-list swvwrquoALMoh11A2Inw'})
+#<div class="fds-ugc-block-mod-list x5TBkLxZnedjkhLhdq8t">
+#<span class="SrXMJafIizWtV9Qivei1"><mark>맥북 에어</mark> 15인치 실버 구매 후기</span>   
+# <a nocr="1" class="lhCgmTnUfae3g5DDMU9c fds-comps-right-image-text-title" target="_blank" data-cb-target="'SYS-0000000035491916.90000003_0000000000000033FF207857'" data-cb-trigger="true"><span class="SrXMJafIizWtV9Qivei1"><mark>맥북 에어</mark> 15인치 실버 구매 후기</span></a>
+    posts = soup.find_all('div', {'class':'fds-ugc-block-mod-list x5TBkLxZnedjkhLhdq8t'})
     for post in posts:
         try:
 
             blog_address_elem = post.find("a", 
-                attrs={"class":"WA5NokotVTFlsTmcyqjA fds-comps-right-image-text-title"}) 
+                attrs={"class":"lhCgmTnUfae3g5DDMU9c fds-comps-right-image-text-title"}) 
             blog_address = blog_address_elem["href"]
-            blog_address_title_elem = post.find("span", attrs={"class":"yzABCkcX2xTtNICntyz1"})
+            blog_address_title_elem = post.find("span", attrs={"class":"SrXMJafIizWtV9Qivei1"})
             blog_address_title = blog_address_title_elem.text 
         except TypeError:
             blog_address = "" 
             blog_address_title = ""
         
-        #<span class="fds-info-sub-inner-text yzABCkcX2xTtNICntyz1">2주 전</span>
-        post_date_elem = post.find('span', {'class':'fds-info-sub-inner-text yzABCkcX2xTtNICntyz1'})
+        #<span class="fds-info-sub-inner-text SrXMJafIizWtV9Qivei1">2024.01.16.</span>
+        post_date_elem = post.find('span', {'class':'fds-info-sub-inner-text SrXMJafIizWtV9Qivei1'})
         post_date = post_date_elem.text if post_date_elem else ""
-        post_title_elem = post.find("a", attrs={"class":"WA5NokotVTFlsTmcyqjA fds-comps-right-image-text-title"})
+        post_title_elem = post.find("a", attrs={"class":"lhCgmTnUfae3g5DDMU9c fds-comps-right-image-text-title"})
         post_title = post_title_elem.text if post_title_elem else "" 
 
         print(blog_address)
